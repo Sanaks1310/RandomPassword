8cde3d356a9b3f8b865f7a1701eb4e0e52e257b1
SANA K S <sanaks1310@gmail.com>
2025-08-07 17:43:43 +0530

    Add files via upload

---
+++ Password.py
@@ -0,0 +1,8 @@
+import random
+import string
+
+pass_len = 12
+charValues = string.ascii_letters + string.digits + string.punctuation
+
+res ="*".join(random.choice(charValues) for i in range(pass_len))
+print(res)
